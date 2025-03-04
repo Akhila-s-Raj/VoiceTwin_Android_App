051c88e833e85ef4825459a32e6c9083a08a0b75
Akhila-s-Raj <akhilasunilraj3@gmail.com>
2025-03-04 15:15:30 +0530

    Firebase connection setup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,10 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("com.android.application") version "8.2.0"
+        id("com.google.gms.google-services") version "4.4.0"
+    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
